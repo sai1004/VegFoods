c0ca4782390838ab9ace0cb1d77aa8ed60ef872d
sai1004 <saikiran.bavandla@gmail.com>
2019-12-05 17:43:28 +0530

    commit

--- src/app/components/our-products/our-products.component.ts
+++ src/app/components/our-products/our-products.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-our-products',
-  templateUrl: './our-products.component.html',
-  styleUrls: ['./our-products.component.css']
+  selector: "app-our-products",
+  templateUrl: "./our-products.component.html",
+  styleUrls: ["./our-products.component.css"]
 })
 export class OurProductsComponent implements OnInit {
+  items: any[] = [
+    {
+      img: "../../../assets/images/product-1.jpg",
+      price: 80,
+      name: "BELL PEPPER",
+      discount: 30
+    },
+    {
+      img: "../../../assets/images/product-1.jpg",
+      price: 80,
+      name: "BELL PEPPER",
+      discount: 30
+    },
+    {
+      img: "../../../assets/images/product-1.jpg",
+      price: 80,
+      name: "BELL PEPPER",
+      discount: 30
+    },
+    {
+      img: "../../../assets/images/product-1.jpg",
+      price: 80,
+      name: "BELL PEPPER",
+      discount: 30
+    },
+    {
+      img: "../../../assets/images/product-1.jpg",
+      price: 80,
+      name: "BELL PEPPER",
+      discount: 30
+    },
+    {
+      img: "../../../assets/images/product-1.jpg",
+      price: 80,
+      name: "BELL PEPPER",
+      discount: 30
+    },
+    {
+      img: "../../../assets/images/product-1.jpg",
+      price: 80,
+      name: "BELL PEPPER",
+      discount: 30
+    },
+    {
+      img: "../../../assets/images/product-1.jpg",
+      price: 80,
+      name: "BELL PEPPER",
+      discount: 30
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/components/show-case-imgs/show-case-imgs.component.ts
+++ src/app/components/show-case-imgs/show-case-imgs.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-show-case-imgs',
-  templateUrl: './show-case-imgs.component.html',
-  styleUrls: ['./show-case-imgs.component.css']
+  selector: "app-show-case-imgs",
+  templateUrl: "./show-case-imgs.component.html",
+  styleUrls: ["./show-case-imgs.component.css"]
 })
 export class ShowCaseImgsComponent implements OnInit {
+  items: any[] = [
+    {
+      img: "../../../assets/images/bg_1.jpg",
+      title: "Fruits"
+    },
+    {
+      img: "",
+      heading: "Vegetables",
+      para: "Protect the health of every home",
+      btn: "shop now"
+    },
+    {
+      img: "../../../assets/images/category-3.jpg",
+      title: "Fruits"
+    },
+    {
+      img: "../../../assets/images/category-2.jpg",
+      title: "Fruits"
+    },
+    {
+      img: "../../../assets/images/bg_1.jpg",
+      title: "Fruits"
+    },
+    {
+      img: "../../../assets/images/category-4.jpg",
+      title: "Fruits"
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
