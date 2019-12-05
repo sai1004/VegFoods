2430d66c092d54d6667c06315320e28637db9ade
sai1004 <saikiran.bavandla@gmail.com>
2019-12-05 19:16:48 +0530

    commit

--- src/app/components/our-products/our-products.component.ts
+++ src/app/components/our-products/our-products.component.ts
@@ -14,43 +14,43 @@ export class OurProductsComponent implements OnInit {
       discount: 30
     },
     {
-      img: "../../../assets/images/product-1.jpg",
+      img: "../../../assets/images/product-2.jpg",
       price: 80,
       name: "BELL PEPPER",
       discount: 30
     },
     {
-      img: "../../../assets/images/product-1.jpg",
+      img: "../../../assets/images/product-3.jpg",
       price: 80,
       name: "BELL PEPPER",
       discount: 30
     },
     {
-      img: "../../../assets/images/product-1.jpg",
+      img: "../../../assets/images/product-4.jpg",
       price: 80,
       name: "BELL PEPPER",
       discount: 30
     },
     {
-      img: "../../../assets/images/product-1.jpg",
+      img: "../../../assets/images/product-5.jpg",
       price: 80,
       name: "BELL PEPPER",
       discount: 30
     },
     {
-      img: "../../../assets/images/product-1.jpg",
+      img: "../../../assets/images/product-6.jpg",
       price: 80,
       name: "BELL PEPPER",
       discount: 30
     },
     {
-      img: "../../../assets/images/product-1.jpg",
+      img: "../../../assets/images/product-7.jpg",
       price: 80,
       name: "BELL PEPPER",
       discount: 30
     },
     {
-      img: "../../../assets/images/product-1.jpg",
+      img: "../../../assets/images/product-8.jpg",
       price: 80,
       name: "BELL PEPPER",
       discount: 30
