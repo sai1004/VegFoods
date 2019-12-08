5e43132cd8b22b93bf60490b3f55ff229fa73741
sai1004 <saikiran.bavandla@gmail.com>
2019-12-08 10:28:40 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import * as AOS from "aos";
+import 'aos/dist/aos.css';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation:ViewEncapsulation.None
+
 })
 export class AppComponent {
   title = 'VegFoods';
+  isShowHeadActive: boolean;
+  ngOnInit() {
+      AOS.init();
+  }
+  constructor(private router: Router) { }
+  onDeactivate() {
+    // document.body.scrollTop = 0;
+    // Alternatively, you can scroll to top by using this other call:
+    window.scrollTo(0, 0)
+}
 }
