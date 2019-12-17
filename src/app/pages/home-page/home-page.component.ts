5dd7ea4ca62271a8dd31e1afb6146dbb00548465
sai1004 <saikiran.bavandla@gmail.com>
2019-12-17 16:31:02 +0530

    commit

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  name:string ='kiran'
+
+
+  items: any[] = [
+    {
+      img: "../../../assets/images/bg_1.jpg",
+      title: "Fruits"
+    },
+    {
+      img: "",
+      heading: "Vegetables",
+      para: "Protect the health of every home",
+      btn: "shop now"
+    },
+    {
+      img: "../../../assets/images/category-3.jpg",
+      title: "Fruits"
+    },
+    {
+      img: "../../../assets/images/category-2.jpg",
+      title: "Fruits"
+    },
+    {
+      img: "../../../assets/images/bg_1.jpg",
+      title: "Fruits"
+    },
+    {
+      img: "../../../assets/images/category-4.jpg",
+      title: "Fruits"
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
