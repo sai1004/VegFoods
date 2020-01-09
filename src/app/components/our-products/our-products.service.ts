1eba67dcf8090d45994335efa7bf0d03171317ae
sai1004 <saikiran.bavandla@gmail.com>
2020-01-09 18:11:48 +0530

    commit

--- src/app/components/our-products/our-products.service.ts
+++ src/app/components/our-products/our-products.service.ts
@@ -11,67 +11,67 @@ export class OurProductsService {
     return [
       {
         id: "4sdfsd4",
-        p_img: "../../../assets/images/product-1.jpg",
-        p_price: 80,
-        p_name: "BELL PEPPER",
-        p_discount: 30,
-        p_quantity: 2
+        img: "../../../assets/images/product-1.jpg",
+        price: 80,
+        name: "BELL PEPPER",
+        discount: 30,
+        isStock: 2
       },
       {
         id: "4sdfsd4",
-        p_img: "../../../assets/images/product-2.jpg",
-        p_price: 120,
-        p_name: "STRAWBERRY",
-        p_discount: 30,
-        p_quantity: 2
+        img: "../../../assets/images/product-2.jpg",
+        price: 120,
+        name: "STRAWBERRY",
+        discount: 30,
+        isStock: 2
       },
       {
         id: "4sdfsd4",
-        p_img: "../../../assets/images/product-3.jpg",
-        p_price: 120,
-        p_name: "GREEN BEANS",
-        p_discount: 30,
-        p_quantity: 2
+        img: "../../../assets/images/product-3.jpg",
+        price: 120,
+        name: "GREEN BEANS",
+        discount: 30,
+        isStock: 2
       },
       {
         id: "4sdfsd4",
-        p_img: "../../../assets/images/product-4.jpg",
-        p_price: 120,
-        p_name: "PURPLE CABBAGE",
-        p_discount: 30,
-        p_quantity: 2
+        img: "../../../assets/images/product-4.jpg",
+        price: 120,
+        name: "PURPLE CABBAGE",
+        discount: 30,
+        isStock: 2
       },
       {
         id: "4sdfsd4",
-        p_img: "../../../assets/images/product-5.jpg",
-        p_price: 80,
-        p_name: "TOMATOE",
-        p_discount: 30,
-        p_quantity: 2
+        img: "../../../assets/images/product-5.jpg",
+        price: 80,
+        name: "TOMATOE",
+        discount: 30,
+        isStock: 2
       },
       {
         id: "4sdfsd4",
-        p_img: "../../../assets/images/product-6.jpg",
-        p_price: 80,
-        p_name: "BROCOLLI",
-        p_discount: 30,
-        p_quantity: 2
+        img: "../../../assets/images/product-6.jpg",
+        price: 80,
+        name: "BROCOLLI",
+        discount: 30,
+        isStock: 2
       },
       {
         id: "4sdfsd4",
-        p_img: "../../../assets/images/product-7.jpg",
-        p_price: 80,
-        p_name: " CARROTS",
-        p_discount: 30,
-        p_quantity: 2
+        img: "../../../assets/images/product-7.jpg",
+        price: 80,
+        name: " CARROTS",
+        discount: 30,
+        isStock: 2
       },
       {
         id: "4sdfsd4",
-        p_img: "../../../assets/images/product-8.jpg",
-        p_price: 80,
-        p_name: "FRUIT JUICE",
-        p_discount: 30,
-        p_quantity: 2
+        img: "../../../assets/images/product-8.jpg",
+        price: 80,
+        name: "FRUIT JUICE",
+        discount: 30,
+        isStock: 2
       }
     ];
   }
